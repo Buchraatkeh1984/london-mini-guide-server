c0273fd13bdbf6b881d294211dc9488dc0df6599
Buchraatkeh1984 <61603626+Buchraatkeh1984@users.noreply.github.com>
2020-09-05 16:35:53 +0100

    level 2commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require('cors')
 var bodyParser = require('body-parser');
-
+var harrowInf= require("./Harrow.json")
 const app = express();
 const port =process.env.PORT|| 3001;
 app.use(cors())
@@ -15,7 +15,23 @@ app.get('/', function(request, response) {
 "/hospitals":"returns hospitals list for stratford"
        
     });
-  
+   
 });
-  
+app.get('/pharmcies', function(request, response) {
+    // response.sendFile(__dirname + '/index.html');
+    response.json(harrowInf.pharmacies)
+});
+app.get('/doctors', function(request, response) {
+    // response.sendFile(__dirname + '/index.html');
+    response.json(harrowInf.doctors)
+});
+
+app.get('/hospitals', function(request, response) {
+    // response.sendFile(__dirname + '/index.html');
+    response.json(harrowInf.hospitals)
+});
+app.get('/colleges', function(request, response) {
+    // response.sendFile(__dirname + '/index.html');
+    response.json(harrowInf.colleges)
+});
   app.listen(port, () => console.log(`[MockServer] listening at http://localhost:${port}`));
